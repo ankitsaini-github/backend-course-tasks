4ba070e44cac370ecee299d293bbf1cceb53f8a6
ankitsaini-github <ankit121saini@gmail.com>
2024-05-03 09:47:55 +0530

    first node server created

--- first-app.js
+++ first-app.js
@@ -1,10 +1,22 @@
-console.log("Hello World!");
+// refresh-----------------
+// console.log("Hello World!");
+// let multiply = (a,b) => a * b;
+// console.log(multiply(5, 5));
+// let student = {
+//   name: "ankit",
+//   branch: "cse",
+// };
+// console.log(student);
+// let fruits = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+// fruits = fruits.map(i=>{
+//   return i===' '? 'empty string':i;
+// })
+// console.log(fruits)
 
-let multiply = (a, b) => a * b;
-console.log(multiply(5, 5));
+const http = require('http');
 
-let student = {
-  name: "ankit",
-  branch: "cse",
-};
-console.log(student);
+const server = http.createServer((req,res)=>{
+  console.log('ankit')
+});
+
+server.listen(4000)
