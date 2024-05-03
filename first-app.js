2f6d4d1e1ee7b8b1ae4a6e9b851e4b6a3a4060dc
ankitsaini-github <ankit121saini@gmail.com>
2024-05-03 12:30:25 +0530

    routing requests

--- first-app.js
+++ first-app.js
@@ -16,7 +16,39 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-  console.log('ankit')
+  console.log(req.url, req.method, req.headers)
+  // process.exit();
+
+  const url=req.url;
+  
+  if(url==='/home'){
+    res.write('<html>')
+    res.write('<head><title>first app</title></head>')
+    res.write('<body><h1>Welcome Home</h1></body>')
+    res.write('</html>')
+    return res.end();
+  }
+  if(url==='/about'){
+    res.write('<html>')
+    res.write('<head><title>first app</title></head>')
+    res.write('<body><h1>Welcome to about us page</h1></body>')
+    res.write('</html>')
+    return res.end();
+  }
+  if(url==='/node'){
+    res.write('<html>')
+    res.write('<head><title>first app</title></head>')
+    res.write('<body><h1>Welcome to node js project</h1></body>')
+    res.write('</html>')
+    return res.end();
+  }
+  
+  res.setHeader('Content-Type','text/html')
+  res.write('<html>')
+  res.write('<head><title>first app</title></head>')
+  res.write('<body><h1>first page</h1></body>')
+  res.write('</html>')
+  res.end();
 });
 
 server.listen(4000)
