407e9c2af3a167752bce28565f5b912927593d94
ankitsaini-github <ankit121saini@gmail.com>
2024-05-04 11:43:28 +0530

    reading and writing from file

--- first-app.js
+++ first-app.js
@@ -13,42 +13,70 @@
 // })
 // console.log(fruits)
 
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-  console.log(req.url, req.method, req.headers)
+const server = http.createServer((req, res) => {
+  // console.log(req.url, req.method, req.headers)
   // process.exit();
 
-  const url=req.url;
-  
-  if(url==='/home'){
-    res.write('<html>')
-    res.write('<head><title>first app</title></head>')
-    res.write('<body><h1>Welcome Home</h1></body>')
-    res.write('</html>')
-    return res.end();
-  }
-  if(url==='/about'){
-    res.write('<html>')
-    res.write('<head><title>first app</title></head>')
-    res.write('<body><h1>Welcome to about us page</h1></body>')
-    res.write('</html>')
-    return res.end();
-  }
-  if(url==='/node'){
-    res.write('<html>')
-    res.write('<head><title>first app</title></head>')
-    res.write('<body><h1>Welcome to node js project</h1></body>')
-    res.write('</html>')
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    const msg = fs.readFileSync('message.txt', 'utf8');
+    res.write("<html>");
+    res.write("<head><title>first app</title></head>");
+    res.write(
+      `<body><p>message = ${msg}</p><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>`
+    );
+    res.write("</html>");
     return res.end();
   }
   
-  res.setHeader('Content-Type','text/html')
-  res.write('<html>')
-  res.write('<head><title>first app</title></head>')
-  res.write('<body><h1>first page</h1></body>')
-  res.write('</html>')
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const msg = parsedBody.split('=')[1];
+      fs.writeFileSync("message.txt", msg);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>first app</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>first app</title></head>");
+    res.write("<body><h1>Welcome to about us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>first app</title></head>");
+    res.write("<body><h1>Welcome to node js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>first app</title></head>");
+  res.write("<body><h1>first page</h1></body>");
+  res.write("</html>");
   res.end();
 });
 
-server.listen(4000)
+server.listen(4000);
