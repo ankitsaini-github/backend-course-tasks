4b7a5376b188134f7126bdceaaf8f4973e50af10
ankitsaini-github <ankit121saini@gmail.com>
2024-05-09 15:49:54 +0530

    sending response

--- first-app.js
+++ first-app.js
@@ -1,10 +1,24 @@
-const http = require("http");
+// const http = require("http");
 
 const express = require("express");
 
 const app = express();
 
-console.log('server started');
-const server = http.createServer();
+app.use((req, res, next)=>{
+  if (req.url === '/favicon.ico') {
+    return res.status(204).end();
+  }
+  console.log('in middleware');
+  next();
+});
 
-server.listen(4000);
+app.use((req, res, next)=>{
+  console.log('in 2nd middleware');
+  res.send('<h1>Hello from Express</h1>');
+});
+
+console.log('server started');
+
+// const server = http.createServer(app);
+// server.listen(4000);
+app.listen(4000);
