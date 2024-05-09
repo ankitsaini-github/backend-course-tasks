8fcd7b9c8729c452295402c51dff3dc34f1eedc3
ankitsaini-github <ankit121saini@gmail.com>
2024-05-09 15:17:53 +0530

    added express js

--- first-app.js
+++ first-app.js
@@ -1,7 +1,10 @@
 const http = require("http");
-const routes = require('./routes')
+
+const express = require("express");
+
+const app = express();
 
 console.log('server started');
-const server = http.createServer(routes);
+const server = http.createServer();
 
 server.listen(4000);
