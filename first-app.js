a375c011ecf17455cdd22835c418309a73336b45
ankitsaini-github <ankit121saini@gmail.com>
2024-05-10 10:32:18 +0530

    post request and body-parser

--- first-app.js
+++ first-app.js
@@ -1,19 +1,33 @@
 // const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next)=>{
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product',(req, res, next)=>{
   if (req.url === '/favicon.ico') {
     return res.status(204).end();
   }
-  console.log('in middleware');
-  next();
+  // console.log('in middleware - add-product');
+  res.send('<form action="/product" method="POST" ><input type="text" name="title" placeholder="title"/><input type="number" name="size" placeholder="size"/><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next)=>{
-  console.log('in 2nd middleware');
+app.post('/product',(req, res, next)=>{
+  if (req.url === '/favicon.ico') {
+    return res.status(204).end();
+  }
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/',(req, res, next)=>{
+  if (req.url === '/favicon.ico') {
+    return res.status(204).end();
+  }
+  // console.log('in middleware - express');
   res.send('<h1>Hello from Express</h1>');
 });
 
